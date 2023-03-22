8947f7fc160ca5d73dea60334a5378efd709577d
haunwo <123608144+haunwo@users.noreply.github.com>
2023-03-22 20:00:14 +0900

    초기 커밋.

---
+++ somamnykinggod/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+using System;
+
+namespace somamnykinggod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
